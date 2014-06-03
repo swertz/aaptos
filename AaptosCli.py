75023e0d921953d61a732674d769d227c7e9f44d
Christophe Delaere <christophe.delaere@cern.ch>
2014-06-03 22:10:46 +0200

    More advanced Power widgets
    Added colors, alarm levels, etc.

--- AaptosCli.py
+++ AaptosCli.py
@@ -3,31 +3,99 @@ from datetime import datetime
 import threading
 import time
         
-#TODO: can change the widget: drop the label and use instead the title.
-class PowerBox(npyscreen.BoxBasic):
+#####################################################
+## Widgets - to be used: PowerBox
+#####################################################
+
+class GaugeWidget(npyscreen.Slider):
+    """A slider with custom label"""
     def __init__(self, screen, *args, **keywords):
+      super(GaugeWidget, self).__init__(screen, *args, **keywords)
+      self.levels = (0,50,100, 100.1)
+      self.unit = "a.u."
+      if 'levels' in keywords:
+        self.levels = keywords['levels']
+      if 'unit' in keywords:
+        self.unit = keywords['unit']
+
+    def translate_value(self):
+      stri = "%4.1f %s" % (self.value, self.unit)
+      return stri
+
+    def set_levels(self,levels):
+      self.lowest = levels[0]
+      self.warning = levels[1]
+      self.maximum = levels[2]
+      self.out_of = levels[3]
+      self.step   = self.out_of/10.
+
+    def get_levels(self):
+      return (self.lowest, self.warning, self.maximum, self.out_of)
+
+    levels = property(get_levels, set_levels)
+
+    def set_value(self, val):
+        #"We can only represent ints or floats, and must be less than what we are out of..."
+        if val is None: val = 0
+        if not isinstance(val, int) and not isinstance(val, float):
+            raise ValueError
+
+        else:
+            self.__value = val
+
+        if self.__value > self.out_of: raise ValueError
+        if hasattr(self, 'levels'):
+          if val < self.levels[1] :
+            self.color = "SAFE"
+          elif val < self.levels[2]:
+            self.color = "WARNING"
+          else:
+            self.color = "DANGER"
+
+    def get_value(self):
+        return float(self.__value)
+
+    value = property(get_value, set_value)
+
+
+class TitleGauge(npyscreen.TitleText):
+    _entry_type = GaugeWidget
+
+    def set_levels(self,levels):
+      self.entry_widget.set_levels(levels)
+
+    def get_levels(self):
+      return self.entry_widget.get_levels()
+
+    levels = property(get_levels, set_levels)
+
+
+class PowerBox(npyscreen.BoxBasic):
+    """A widget combining Voltage and Current gauges"""
+
+    def __init__(self, screen, *args, **keywords):
+        """Initialize the widget. Relevant parameters are name, values, and levels.
+           Levels is a pair of triplet (min, warning, max)."""
         super(PowerBox, self).__init__(screen, *args, **keywords)
+        if 'levels' in keywords:
+            self.levels_ = keywords['levels']
+        else:
+            self.levels_ = [(0,5,5,5), (0,0.5,1,2)]
         self.make_contained_widgets()
-        if 'value' in keywords:
-            self.value = keywords['value']
         if 'values' in keywords:
             self.values = keywords['values']
     
     def make_contained_widgets(self):
         self._my_widgets = []
-        labels = ["","Voltage","Current"]
-        contained_widgets = [ npyscreen.FixedText, npyscreen.TitleSlider, npyscreen.TitleSlider ]
+        labels = ["Voltage","Current"]
+        units  = ["V","A"]
+        contained_widgets = [ TitleGauge, TitleGauge ]
         for i,w in enumerate(contained_widgets):
-          self._my_widgets.append(w(self.parent, 
-           rely=self.rely+1+i, relx = self.relx+2, 
-           max_width=self.width-4, max_height=self.height-2,
-           name = labels[i]))
+          self._my_widgets.append(w(self.parent, rely=self.rely+1+i, relx = self.relx+2, 
+                                    max_width=self.width-4, max_height=self.height-2, name = labels[i], unit=units[i], levels=self.levels_[i]))
           self._my_widgets[-1].editable=False
           self._my_widgets[-1].scroll_exit=False
           self._my_widgets[-1].slow_scroll=False
-        self.entry_widget = self._my_widgets[0]
-        self._my_widgets[1].name = "Voltage"
-        self._my_widgets[2].name = "Current"
             
     def update(self, clear=True):
         if self.hidden and clear:
@@ -50,45 +118,46 @@ class PowerBox(npyscreen.BoxBasic):
         self.editing=False
         self.display()
         
-    def get_value(self):
-        if hasattr(self, 'entry_widget'):
-            return self.entry_widget.value
-        elif hasattr(self, '__tmp_value'):
-            return self.__tmp_value
-        else:
-            return None
-
-    def set_value(self, value):
-        if hasattr(self, 'entry_widget'):
-            self.entry_widget.value = value
-        else:
-            # probably trying to set the value before the textarea is initialised
-            self.__tmp_value = value
-
-    def del_value(self):
-        del self.entry_widget.value
-    value = property(get_value, set_value, del_value)
-    
-#TODO: use values[3,4] to store max voltage and max current.
     def get_values(self):
-        if hasattr(self, 'entry_widget'): 
-            return [self._my_widgets[1].value, self._my_widgets[2].value ]
-        elif hasattr(self, '__tmp_value'):
+        if hasattr(self, '_my_widgets'): 
+            return [self._my_widgets[0].value, self._my_widgets[1].value ]
+        elif hasattr(self, '__tmp_values'):
             return self.__tmp_values
         else:
             return None
-    def set_values(self, value):
-        if hasattr(self, 'entry_widget'): 
-            self._my_widgets[1].value = value[0]
-            self._my_widgets[2].value = value[1]
-        elif hasattr(self, '__tmp_value'):
+
+    def set_values(self, values):
+        if hasattr(self, '_my_widgets'): 
+            self._my_widgets[0].value = values[0]
+            self._my_widgets[1].value = values[1]
+        elif hasattr(self, '__tmp_values'):
             # probably trying to set the value before the textarea is initialised
-            self.__tmp_values = value
+            self.__tmp_values = values
+
     def del_values(self):
+        del self._my_widgets[0].value
         del self._my_widgets[1].value
-        del self._my_widgets[2].value
+
     values = property(get_values, set_values, del_values)
     
+    def get_levels(self):
+        if hasattr(self, '_my_widgets'): 
+            return [self._my_widgets[0].get_levels(), self._my_widgets[1].get_levels() ]
+        elif hasattr(self, '__tmp_levels'):
+            return self.__tmp_levels
+        else:
+            return None
+
+    def set_levels(self, levels):
+        if hasattr(self, '_my_widgets'): 
+            self._my_widgets[0].set_levels(levels[0])
+            self._my_widgets[1].set_levels(levels[1])
+        elif hasattr(self, '__tmp_levels'):
+            # probably trying to set the value before the textarea is initialised
+            self.__tmp_levels = levels
+
+    levels = property(get_levels, set_levels)
+
     def get_editable(self):
         return False
 
@@ -96,14 +165,17 @@ class PowerBox(npyscreen.BoxBasic):
         pass
 
     def del_editable(self):
-        del self.entry_widget.editable
+        pass
+
     editable = property(get_editable, set_editable, del_editable)
 
-
-    # This application class serves as a wrapper for the initialization of curses
-    # and also manages the actual forms of the application
+#####################################################
+## App and Forms
+#####################################################
 
 class MyTestApp(npyscreen.NPSAppManaged):
+    """This application class serves as a wrapper for the initialization of curses
+       and also manages the actual forms of the application"""
 
     keypress_timeout_default = 10
 
@@ -111,9 +183,10 @@ class MyTestApp(npyscreen.NPSAppManaged):
         #self.registerForm("MAIN", MainForm(name = "Welcome to Aaptos"))
         self.addForm("MAIN", MainForm , name = "Welcome to Aaptos")
 
-# This form class defines the display that will be presented to the user.
 
 class MainForm(npyscreen.Form):
+#class MainForm(npyscreen.FormBaseNewWithMenus):
+    """This form class defines the display that will be presented to the user."""
 
     def update_clock(self,period=1):
         while True:
@@ -132,11 +205,11 @@ class MainForm(npyscreen.Form):
 
         # name, voltage, current
         rely = self.nextrely
-        self.PS1 = self.add(PowerBox, value="P6V",  values=[5,1.2], width=50, height=5, rely=rely)
-        self.PS2 = self.add(PowerBox, value="P25V", values=[5,1.2], width=50, height=5, rely=rely, relx=55)
+        self.PS1 = self.add(PowerBox, name="P6V",  levels=[(0,5,5,6),(0,1,1.5,2)], values=[5,1.2], width=50, height=4, rely=rely)
+        self.PS2 = self.add(PowerBox, name="P25V", levels=[(0,5,5,6),(0,1,1.5,2)], values=[5,1.2], width=50, height=4, rely=rely, relx=55)
         rely = self.nextrely
-        self.PS3 = self.add(PowerBox, value="M25V", values=[5,1.2], width=50, height=5, rely=rely)
-        self.PS4 = self.add(PowerBox, value="P20V", values=[5,1.2], width=50, height=5, rely=rely, relx=55)
+        self.PS3 = self.add(PowerBox, name="M25V", levels=[(0,6,6,6),(0,1,1.5,2)], values=[5,1.2], width=50, height=4, rely=rely)
+        self.PS4 = self.add(PowerBox, name="P20V", levels=[(0,6,6,6),(0,1,1.5,2)], values=[5,1.2], width=50, height=4, rely=rely, relx=55)
         self.nextrely += 2
 
         # options
@@ -149,7 +222,7 @@ class MainForm(npyscreen.Form):
     def afterEditing(self):
         self.parentApp.setNextForm(None)
 
-#TODO: add menu: settings (I,V for each, limits, etc.) , save, recall, quit
+#TODO: add menu: settings (I,V for each, limits, etc.), SOAP server, save, recall, quit
 
 #TODO: add the method that reacts to changes in widgets and sends SOAP commands accordingly (enable/disable, lock
 # also control the logger... this uses a threading.Event... how to communicate "outside"?
